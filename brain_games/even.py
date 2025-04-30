3c07c7c6f63fcf9dc17ab56d2433e0f9a19d0d00
krll.antv <antv.krll@gmail.com>
2025-05-01 00:22:14 +0300

    fix even.py

--- brain_games/even.py
+++ brain_games/even.py
@@ -16,7 +16,7 @@ def even_game():
     score = 3
     index = 0
     name = prompt.string("May I have your name? ")
-    print(f'Hello {name}')
+    print(f'Hello, {name}!')
     print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
     while index < score:
         random_numb = random.randint(1, 100)
