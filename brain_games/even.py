3e15d21fd1a57db012bb7922335b585f1b00ed58
krll.antv <antv.krll@gmail.com>
2025-04-30 18:41:40 +0300

    fix code in even.py

--- brain_games/even.py
+++ brain_games/even.py
@@ -3,7 +3,7 @@ import random
 import prompt
 
 
-def even_numb(numb):
+def even_numb(numb: int):
     result = ''
     if numb % 2 == 0:
         result = 'yes'
@@ -16,21 +16,21 @@ def even_game():
     score = 3
     index = 0
     name = prompt.string("May I have your name? ")
-    print("Hello, " + name)
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print(f'Hello {name}')
+    print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
     while index < score:
         random_numb = random.randint(1, 100)
-        print('Question answer:', random_numb)
+        print(f"Question answer: {random_numb}")
         result = prompt.string("Your answer: ", empty=True)
         if result == even_numb(random_numb):
             print("Currect!")
         else:
-            print("\'" + result + "\'", 'is wrong answer. Correct answer was \''
-            + even_numb(random_numb) + "\'.\nLet's try again,", name + "!")
+            print(
+            f"'{result}' is wrong answer."
+            f" Correct answer was '{even_numb(random_numb)}\n"
+            f"Let's try again, {name}!"
+            )
             break
         index += 1
     if index == 3:
-        print("Congratulations,", name + "!")
-
-
-
+        print(f"Congratulations {name}!")
