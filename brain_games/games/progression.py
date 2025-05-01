249fc78f14c8a38b32cd7cf15ecb104a5c4f35be
krll.antv <antv.krll@gmail.com>
2025-05-01 18:37:00 +0300

    add progression.py

---
+++ brain_games/games/progression.py
@@ -0,0 +1,21 @@
+import random
+
+def welcome():
+    print("What number is missing in th progression?")
+
+
+def question_answer():
+    numb = random.randint(5, 10)
+    random_step = random.randint(2, 5)
+    index = 0
+    progress = []
+    while len(progress) < numb:
+        progress.append(numb + index)
+        index += random_step   
+    answer = random.choice(progress)
+    ind = progress.index(answer)
+    progress[ind] = '..'
+    string = ' '.join(map(str, progress))
+    question = f'Question: {string}'
+    result = (question, str(answer))
+    return result
