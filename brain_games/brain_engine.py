3ab48dc3a2cd33cc2ba5241578b00b7db99cc306
krll.antv <antv.krll@gmail.com>
2025-05-01 14:25:17 +0300

    add brain_engine.py

---
+++ brain_games/brain_engine.py
@@ -0,0 +1,28 @@
+import prompt
+
+# NUMB ROUND
+LVL = 3
+
+# welcome message
+def game_run(module):
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name)
+    # run welcome function from incoming module
+    module.welcome()
+    
+    for lvl_char in range(LVL):
+        # unpacking quest and answer funct. from incoming module
+        quest_answer = module.question_answer()
+        (question, answer) = quest_answer
+        print(question)
+        result = prompt.string("Your answer: ", empty=True)
+        if result == answer:
+            print("Currect!")
+        else:
+            print(
+            f"'{result}' is wrong answer."
+            f" Correct answer was '{answer}'\n"
+            f"Let's try again, {name}!"
+            )
+        print(f"Congratulations, {name}!")
